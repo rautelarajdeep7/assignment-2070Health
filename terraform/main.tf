83c71c691b820fe11721c590a3d7b5937e25c990
rautelarajdeep7 <Rautelarajdeep7@gmail.com>
2024-09-17 01:50:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_subnet" "subnet-public" {
   vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.1./24"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 }
 
